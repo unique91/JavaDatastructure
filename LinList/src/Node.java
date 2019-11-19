e11dded755f695e0d357ba21825d62643ef0b9a8
unique91 <jsaldan38@gmail.com>
2019-11-19 11:20:57 +0100

    create new class Node

--- LinList/src/Node.java
+++ LinList/src/Node.java
@@ -1,4 +1,15 @@
 
 public class Node {
-
+	Object data;
+	Node next;
+	
+	Node(Object data) {
+		this.data = data;
+		next = null;
+	}
+	
+	private Object getFirstElement() {
+		return this.data;
+	}
+	
 }
