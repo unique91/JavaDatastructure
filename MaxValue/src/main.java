3a87da75dfa710c1069853d14bbc0f2f0b186462
unique91 <jsaldan38@gmail.com>
2019-11-19 10:57:17 +0100

    create the max-value algorithm to find the max in the array

--- MaxValue/src/main.java
+++ MaxValue/src/main.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+				
+		System.out.println("Length of Array?");
+		int len = scanner.nextInt();
+		
+		int[] arr = new int [len];
+		// create dynamic array and fill it
+		int i = 0;
+		while(i < len) {
+			System.out.println("Enter a Number");
+			int input = scanner.nextInt();
+			arr[i] = input;
+			i++;
+		}
+		
+		// output the array
+		System.out.print("|");
+		for(int j = 0; j < len; j++) {
+			System.out.print(arr[j] + " | ");
+		}
+		System.out.println();
+		
+		// search maximum in the array
+		int maxValue = arr[0];
+		for(int k = 1; k < len; k++) {
+			if(maxValue < arr[k]) 
+				maxValue = arr[k];
+		}
+		System.out.println("Maximum in Array are: " + maxValue);
 	}
 
 }
