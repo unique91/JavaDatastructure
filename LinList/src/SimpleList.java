17d7dfeeccc0ac86905c37fc74c5470355e9898f
unique91 <jsaldan38@gmail.com>
2019-11-19 12:22:30 +0100

    some changes in insertLast method

--- LinList/src/SimpleList.java
+++ LinList/src/SimpleList.java
@@ -1,41 +1,78 @@
-import com.sun.xml.internal.bind.marshaller.NoEscapeHandler;
 
-public class SimpleList{
-	class Node {
-		private Object data;
-		private Node next;
-		
-		Node(Object data) {
-			this.data = data;
-			this.next = null;
-		}
-		
-		public void setNextLement(Node next) {
-			this.next = next;
-		}
-		
-		public Node getLastElement() {
-			return this.next;
-		}
-		
-		public Object getObject() {
-			return this.data;
-		}
+public class SimpleList implements ListOperation {
+
+	public SimpleList() {}
+	
+	Node firstElementNode = new Node("Head");
+	
+	public void insertLastElement(Object data) {
+		Node newNode = new Node(data);
+		Node lastNode = null;
+		lastNode.getLastElement();
+		lastNode.setNextLement(newNode);
 	}
 
-	
-	
-	
-	/*Node node = new Node(null);
-	
+	@Override
 	public void insertLast(Object data) {
 		Node newNode = new Node(data);
-		Node lastNode = node.getNextElement();
-		lastNode.setNextElement(newNode);
+		Node lastNode = 
+	}
+
+	@Override
+	public void insertFirst(Node node, Object data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteElement() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean find(Object data) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Node getFirstElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Node getLastElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void outputList() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
+class Node {
+	private Object data;
+	private Node next;
+	
+	Node(Object data) {
+		this.data = data;
+		this.next = null;
 	}
 	
-	public void insertElement(Object data) {
-		
-	}*/
+	public void setNextLement(Node next) {
+		this.next = next;
+	}
 	
+	public Node getLastElement() {
+		return this.next;
+	}
+	
+	public Object getObject() {
+		return this.data;
+	}
 }
