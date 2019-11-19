e0391281074327b69471285403b97d540fccef5b
unique91 <jsaldan38@gmail.com>
2019-11-19 11:50:13 +0100

    change Node settings and create new methods

--- LinList/src/Node.java
+++ LinList/src/Node.java
@@ -1,15 +1,22 @@
 
 public class Node {
-	Object data;
-	Node next;
+	private Object data;
+	private Node next;
 	
 	Node(Object data) {
 		this.data = data;
 		next = null;
 	}
 	
-	private Object getFirstElement() {
-		return this.data;
+	public void setNextElement(Node next) {
+		this.next = next;
 	}
 	
+	public Node getNextElement() {
+		return this.next;
+	}
+	
+	public Object getObject() {
+		return this.data;
+	}
 }
