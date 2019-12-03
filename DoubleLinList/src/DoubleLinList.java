df17b90d0067f3556c48e9ece01b0952d28ef20b
unique91 <jsaldan38@gmail.com>
2019-12-03 16:20:49 +0100

    create class DoubleLinList which will include all List methods

---
+++ DoubleLinList/src/DoubleLinList.java
@@ -0,0 +1,51 @@
+public class DoubleLinList  implements List {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isInList(Object x) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object firstElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List insert(Object x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List append(Object x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List delete(Object x) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List delete() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
