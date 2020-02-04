4f41518f7ae3c060967c07ab122025dedaca5963
unique91 <jsaldan38@gmail.com>
2020-02-04 18:41:26 +0100

    Initialization of adjacency matrix

---
+++ Graph/src/AGraph.java
@@ -0,0 +1,13 @@
+// Define a Graph with Adjazenzmatrix
+// Only Boolean values
+public class AGraph {
+	String[] nodeDescribe = {"a", "b", "c", "d", "e", "f", "g", "h"};
+	int nodeSize;
+	//matrix[u][v] <=> (u,v) elem. E
+	boolean[][] matrix = {{false, true, false, true, true, false, false, false},
+						  {false, false, true, false, true, false, false, false}};    
+	
+	AGraph(String[] nodeDescribe, boolean[][] node) {
+		
+	}
+}
